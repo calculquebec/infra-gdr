2c3b02d19711606543c789cbe5e71218d0f5fe96
opsocket <opsocket@pm.me>
2023-04-24 23:47:44 -0400

    📝 initial sphinx documentation

---
+++ docs/conf.py
@@ -0,0 +1,86 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Gestion des Données de Recherche (GDR)'
+copyright = '2023, opsocket'
+author = 'opsocket'
+
+version = '0.0.1'
+release = version
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+  'sphinx.ext.autodoc',
+  'sphinx.ext.intersphinx',
+  'sphinx.ext.autosectionlabel',
+  'sphinx_rtd_theme',
+  'myst_parser',
+]
+
+autosectionlabel_prefix_document = True
+
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+source_suffix = ['.rst', '.md']
+
+pygments_style = 'monokai'
+
+todo_include_todos = False
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+
+html_context = {
+  "display_gitlab": True,
+  "gitlab_host": "git.computecanada.ca",
+  "gitlab_user": "opsocket",
+  "gitlab_repo": "infra-gdr",
+  "gitlab_version": "main",
+  "conf_py_path": "/docs/", # path in the checkout to the docs root
+}
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+  'logo_only': False,
+  'display_version': True,
+  'prev_next_buttons_location': 'bottom',
+  'style_external_links': True,
+  'vcs_pageview_mode': 'blob',
+  'collapse_navigation': True,
+  'sticky_navigation': True,
+  'navigation_depth': 4,
+  'titles_only': False,
+}
+
+
+html_static_path = ['_static']
+
+html_css_files = ['css/extra.css']
+
+html_show_sphinx = False
---
+++ docs/troubleshooting.md
@@ -0,0 +1,25 @@
+# Troubleshooting
+
+If you encounter issues with deploying the research data management services, please try the following troubleshooting steps:
+
+## Verify Input Variables
+
+If you are experiencing issues with deploying the services, it may be because of incorrectly defined input variables. Ensure that all the required input variables have been correctly defined in `variables.tf`. Review the documentation for each input variable and verify that the values provided for them are correct.
+
+## Check Provider Versions
+
+Terraform providers have their own versioning and compatibility requirements. Ensure that the required provider versions have been correctly defined in `providers.tf` and `versions.tf`. You can check the documentation for each provider to verify the specific versions required.
+
+## Verify Infrastructure Resources
+
+If the input variables and provider versions have been correctly defined, the next step is to verify that the required infrastructure resources have been correctly defined in `apps.tf`, `databases.tf`, and `firewall.tf`. Check each resource definition and ensure that the configuration is correct.
+
+## Verify External Scripts and Files
+
+If any external scripts or files are required by the project, ensure that they have been correctly configured and placed in the `external` directory. Verify that the permissions and ownerships of any files in this directory are correctly set.
+
+## Check Logs and Error Messages
+
+If you are still experiencing issues after trying the above troubleshooting steps, check the logs and error messages for any specific issues or errors that need to be addressed. You can use the `terraform plan` and `terraform apply` commands with the `-debug` flag to get more detailed logging information.
+
+If you are unable to resolve the issues after reviewing the logs and error messages, consult the Terraform documentation or seek help from the Terraform community forums.
