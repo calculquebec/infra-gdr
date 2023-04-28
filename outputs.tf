1537395fb957296ff770ea7bbb817ecdf1a15e08
opsocket <opsocket@pm.me>
2023-04-28 18:09:19 -0400

    💚 update ci

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "endpoints" {
-  description = "Endpoints for the research group"
-  value = {
-    mattermost = "${local.workspace}.chat.cirst.ca"
-    nextcloud  = "${local.workspace}.cloud.cirst.ca"
-  }
-}
