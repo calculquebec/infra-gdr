b065e9469935a4d7da6ec1cbb52ad1db8b5c90ed
opsocket <opsocket@pm.me>
2023-05-01 11:31:36 -0400

    💚 fix typos

--- firewall.tf
+++ firewall.tf
@@ -5,22 +5,22 @@ resource "openstack_networking_secgroup_v2" "apps" {
 }
 
 resource "openstack_networking_secgroup_rule_v2" "http" {
-  direction     = "ingress"
-  ethertype     = "IPv4"
-  protocol      = "tcp"
-  port_range_min = 80
-  port_range_max = 80
-  remote_ip_prefix = var.INTERNAL_GATEWAY_IPV4
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = var.INTERNAL_GATEWAY_IPV4
   security_group_id = openstack_networking_secgroup_v2.apps.id
 }
 
 resource "openstack_networking_secgroup_rule_v2" "https" {
-  direction     = "ingress"
-  ethertype     = "IPv4"
-  protocol      = "tcp"
-  port_range_min = 443
-  port_range_max = 443
-  remote_ip_prefix = var.INTERNAL_GATEWAY_IPV4
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 443
+  port_range_max    = 443
+  remote_ip_prefix  = var.INTERNAL_GATEWAY_IPV4
   security_group_id = openstack_networking_secgroup_v2.apps.id
 }
 
--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "INTERNAL_GATEWAY_IPV4" {
-  type = string
+  type        = string
   description = "The internal gateway for HTTP(S) traffic"
 }
 
 variable "ENVIRONMENT_NAME" {
-  type = string
+  type        = string
   description = "The name that identifies the cluster.\nMust start with a letter or number, and can contain letters, numbers, hyphens, and underscores.\nRead more at https://www.rfc-editor.org/rfc/rfc3986"
   validation {
-    condition = can(regex("^[a-zA-Z0-9][a-zA-Z0-9-_]*[a-zA-Z0-9]$", var.cluster_name))
+    condition     = can(regex("^[a-zA-Z0-9][a-zA-Z0-9-_]*[a-zA-Z0-9]$", var.ENVIRONMENT_NAME))
     error_message = "The cluster name must start with a letter or number, and can contain letters, numbers, hyphens, and underscores."
   }
 }
