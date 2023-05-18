5afd6db074be76b0031d2a6a87c9d86f08a8e54a
opsocket <opsocket@pm.me>
2023-05-17 21:38:30 -0400

    📝 format

--- ansible.tf
+++ ansible.tf
@@ -31,10 +31,10 @@ resource "ansible_host" "apps" {
   name   = openstack_compute_instance_v2.apps.name
   groups = ["apps"]
   variables = {
-    ansible_user                 = "ubuntu",
-    ansible_host                 = openstack_compute_instance_v2.apps.access_ip_v4,
+    ansible_user = "ubuntu",
+    ansible_host = openstack_compute_instance_v2.apps.access_ip_v4,
     # ansible_ssh_private_key_file = "{{ ssh_identity_file }}",
-    ansible_ssh_common_args      = "-J {{ ansible_user }}@${data.openstack_networking_floatingip_v2.gateway.address}:${var.ssh_proxy_port} -o StrictHostKeyChecking=no"
+    ansible_ssh_common_args = "-J {{ ansible_user }}@${data.openstack_networking_floatingip_v2.gateway.address}:${var.ssh_proxy_port} -o StrictHostKeyChecking=no"
   }
 }
 
@@ -45,11 +45,11 @@ resource "ansible_host" "databases" {
   name   = module.phac.databases[count.index].name
   groups = ["databases"]
   variables = {
-    ansible_user                 = "ubuntu",
-    ansible_host                 = module.phac.databases[count.index].access_ip_v4,
+    ansible_user = "ubuntu",
+    ansible_host = module.phac.databases[count.index].access_ip_v4,
     # ansible_ssh_private_key_file = "{{ ssh_identity_file }}",
-    ansible_ssh_common_args      = "-J {{ ansible_user }}@${data.openstack_networking_floatingip_v2.gateway.address}:${var.ssh_proxy_port} -o StrictHostKeyChecking=no"
-    repmgr_node_id               = "${count.index + 1}"
-    repmgr_node_name             = "node{{ repmgr_node_id }}"
+    ansible_ssh_common_args = "-J {{ ansible_user }}@${data.openstack_networking_floatingip_v2.gateway.address}:${var.ssh_proxy_port} -o StrictHostKeyChecking=no"
+    repmgr_node_id          = "${count.index + 1}"
+    repmgr_node_name        = "node{{ repmgr_node_id }}"
   }
 }
